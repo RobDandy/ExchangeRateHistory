f87dee471dc7cf9983fd5a3e40ba125beced105b
Rob Dandy <robdandy@soholighting.com>
2021-11-21 17:16:04 +0000

    Final code cleanup

--- ExchangeRateHistory/Controllers/ExchangeRateController.cs
+++ ExchangeRateHistory/Controllers/ExchangeRateController.cs
@@ -2,45 +2,44 @@
 using ExchangeRateHistory.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+namespace ExchangeRateHistory.API.Controllers;
 
-namespace ExchangeRateHistory.API.Controllers {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class ExchangeRateController : ControllerBase {
-        private readonly IExchangeRateService _exchangeRateService;
+[Route("api/[controller]")]
+[ApiController]
+public class ExchangeRateController : ControllerBase {
+    private readonly IExchangeRateService _exchangeRateService;
 
-        public ExchangeRateController(IExchangeRateService exchangeRateService) {
-            _exchangeRateService = exchangeRateService;
+    public ExchangeRateController(IExchangeRateService exchangeRateService) {
+        _exchangeRateService = exchangeRateService;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetHistoricalRates(string baseCurrency, string targetCurrency, [FromQuery] DateTime[] dates) {
+        if (dates == null) {
+            return BadRequest(new { Error = "No dates provided to constructor" });
         }
 
-        [HttpGet]
-        public async Task<IActionResult> GetHistoricalRates(string baseCurrency, string targetCurrency, [FromQuery] DateTime[] dates) {
-            if (dates == null) {
-                return BadRequest(new { Error = "No dates provided to constructor" });
-            }
+        IEnumerable<ExchangeRate> rates;
 
-            IEnumerable<ExchangeRate> rates;
-
-            try {
-                rates = await _exchangeRateService.GetRates(baseCurrency, targetCurrency, dates);
-            } catch (NullReferenceException ex) {
-                return BadRequest(ex.Message);
-            } catch (Exception ex) {
-                // Logging
-                Console.WriteLine($"{ex.Message} \n {ex.StackTrace}");
-                return BadRequest("An unknown error occurred");
-            }
-
-            AverageExchangeRate averageRate; 
-            
-            try {
-                averageRate = AverageExchangeRate.Create(rates);
-            } catch (ArgumentNullException) {
-                return NotFound(new { Error = "No rates found for specified currencies and dates" });
-            }
-
-            return Ok(averageRate);
+        try {
+            rates = await _exchangeRateService.GetRates(baseCurrency, targetCurrency, dates);
+        } catch (NullReferenceException ex) {
+            return BadRequest(ex.Message);
+        } catch (Exception ex) {
+            // Logging
+            Console.WriteLine($"{ex.Message} \n {ex.StackTrace}");
+            return BadRequest("An unknown error occurred");
         }
+
+        AverageExchangeRate averageRate;
+
+        try {
+            averageRate = AverageExchangeRate.Create(rates);
+        } catch (ArgumentNullException) {
+            return NotFound(new { Error = "No rates found for specified currencies and dates" });
+        }
+
+        return Ok(averageRate);
     }
 }
+
--- ExchangeRateHistory/Program.cs
+++ ExchangeRateHistory/Program.cs
@@ -21,8 +21,7 @@ builder.Services.AddHttpClient<IExchangeRateService, ExchangeRateHostService>(cl
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+if (app.Environment.IsDevelopment()) {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
